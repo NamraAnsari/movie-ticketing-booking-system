f69e58c09b92df537d3248f95cd773fc1bd46a57
NamraAnsari <namraansari6797@gmail.com>
2018-10-12 14:00:28 +0530

    Added css and js folders

---
+++ js/myScript.js
@@ -0,0 +1,19 @@
+function openForm1() {
+  document.getElementById('myForm1').style.display = 'block';
+}
+
+function closeForm1() {
+  document.getElementById('myForm1').style.display = 'none';
+}
+
+function openForm2() {
+  document.getElementById('myForm2').style.display = 'block';
+}
+
+function closeForm2() {
+  document.getElementById('myForm2').style.display = 'none';
+}
+
+document.getElementById('tush').addEventListener('click', function(){
+    alert("hello ! !!! !!")
+});
